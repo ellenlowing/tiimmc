c92418ceb7a35a736abb9a35fca3d7cfd116b1c1
ellenlowing <lowing@bu.edu>
2019-07-11 23:57:42 -0400

    change countdown to 00:00:00:00

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var mode = 1; // 1: normal, 0: screensaver (idle)
 var idleTimeout;
-var idleInterval = 1000; //90000
+var idleInterval = 90000; //90000
 var mobileMode = false;
 var touchInit = false; // mobile only
 var touchTimeout;
@@ -227,10 +227,10 @@ function idle () {
     var hours = Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((dist % (1000 * 60)) / 1000);
-    $('#days').html(days<10?'0'+days:days);
-		$('#hours').html(hours<10?'0'+hours:hours);
-		$('#minutes').html(minutes<10?'0'+minutes:minutes);
-		$('#seconds').html(seconds<10?'0'+seconds:seconds);
+    $('#days').html(days<0?'00':days<10?'0'+days:days);
+		$('#hours').html(hours<0?'00':hours<10?'0'+hours:hours);
+		$('#minutes').html(minutes<0?'00':minutes<10?'0'+minutes:minutes);
+		$('#seconds').html(seconds<0?'00':seconds<10?'0'+seconds:seconds);
   });
 }
 
